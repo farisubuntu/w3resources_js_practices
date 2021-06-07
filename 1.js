29f2607aeb38110acadfb14ab3b47fb05a48aeda
farisubuntu <farisubuntu@gmail.com>
2021-06-07 20:22:48 +0300

    Initial commit

---
+++ 1.js
@@ -0,0 +1,7 @@
+console.log("1.js");
+// 1. Write a JavaScript program to display the current day and time in the following format.  Go to the editor
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+var today = Date.today();
+console.log(today);
